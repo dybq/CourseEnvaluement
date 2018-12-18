ca2da387805d26320d72f0a7732a0203593a8f5e
puring <784932562@qq.com>
2018-12-18 15:40:07 +0800

    111

--- AutoEnveluement.py
+++ AutoEnveluement.py
@@ -2,12 +2,14 @@ import time
 import json
 from selenium import webdriver
 
-right = ["非常同意", "一定会", "对我帮助大", "4次以上", "4小时以上", "每次", ">12小时","非常同意 ", "一定会 ", "对我帮助大 ", "4次以上 ", "4小时以上 ", "每次 ", ">12小时 "]
+right = ["非常同意", "一定会", "对我帮助大", "4次以上", "4小时以上", "每次", ">12小时", "非常同意 ", "一定会 ", "对我帮助大 ", "4次以上 ", "4小时以上 ", "每次 ",
+         "5次及以上","5次及以上 ",
+         ">12小时 "]
 a = open("user.txt")
 data = a.readlines()
 a.close()
 driver_option = webdriver.ChromeOptions()
-#driver_option.add_argument("--headless")
+# driver_option.add_argument("--headless")
 driver = webdriver.Chrome("./chromedriver", chrome_options=driver_option, )
 driver.get("http://kcpg.pku.edu.cn/")
 driver.maximize_window()
@@ -25,41 +27,44 @@ for page in range(1, 11):
     p = driver.find_element_by_xpath(f'//*[@id="myTaskContainer"]/table/tbody[2]/tr[{page}]/td[7]/a')
 
     driver.execute_script("arguments[0].scrollIntoView();", p)
+    # 挪到第n项
     p.click()
     time.sleep(3)
-    print(driver.window_handles)
     driver.switch_to.window(driver.window_handles[1])
     driver.maximize_window()
+    # 最大化
     tags = driver.find_elements_by_tag_name("input")
     # 获取所有可点击
     atags = driver.find_elements_by_xpath('//*[@id="divHeader"]/div[3]/div/ul/li')
     for a in atags:
         # 点击各个老师
 
-        driver.execute_script('arguments[0].scrollIntoView();', a)
+        driver.execute_script('arguments[0].focus();', a)
 
         a.click()
 
         for i in tags:
             try:
                 driver.execute_script('arguments[0].removeAttribute(\"style\")', i)
-                good = eval(str(i.get_attribute("options")))
-                print(good['Title'])
+
+                driver.execute_script("arguments[0].focus();", i)
+
+                # input可见
+
+                good = json.loads(i.get_attribute('options'))
                 for j in right:
 
                     if j == good['Title']:
-
                         i.click()
-                        time.sleep(1)
+                        print(1)
                         break
-
-                driver.execute_script("arguments[0].scrollIntoView();", i)
             except:
-                driver.execute_script("arguments[0].scrollIntoView();", i)
-
                 continue
+
+
     driver.find_element_by_xpath('//*[@id="btnSave"]').click()
+    time.sleep(2)
     driver.find_element_by_xpath('/html/body/div[5]/div/div[2]/div[4]/button[1]').click()
-    time.sleep(5)
+    time.sleep(3)
     driver.close()
     driver.switch_to.window(mainPage)
